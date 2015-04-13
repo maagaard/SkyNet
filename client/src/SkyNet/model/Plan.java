5758afc5950edd3e71f65b80a9d06bde8bdee430
Bjarke Vad <bjarke.vad90@gmail.com>
2015-04-13 16:39:47 +0200

    Added Plan model

--- client/src/SkyNet/model/Plan.java
+++ client/src/SkyNet/model/Plan.java
@@ -1,4 +1,15 @@
 package SkyNet.model;
 
+import SkyNet.Command;
+
+import java.util.LinkedList;
+
 public class Plan {
+    private LinkedList<Command> plan;
+
+    public LinkedList<Command> GetPlan() { return plan; }
+
+    public Plan(LinkedList<Command> _plan) {
+        plan = _plan;
+    }
 }
