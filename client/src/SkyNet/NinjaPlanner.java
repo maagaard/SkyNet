7306f03885caa021c0f6a3b160be28cb66531329
michel <michel@randahl.net>
2015-04-14 16:59:18 +0200

    working on HTN

--- client/src/SkyNet/NinjaPlanner.java
+++ client/src/SkyNet/NinjaPlanner.java
@@ -6,7 +6,7 @@ import SkyNet.model.Plan;
 public class NinjaPlanner implements Planner {
     @Override
     public Plan createPlan(Level level) {
-        Node initNode = new PartialPlanNode(level, LevelReader)
+        //Node initNode = new PartialPlanNode(level, LevelReader)
 
         return null;
     }
