49f4aefd5a2e1e0ca8da35c4f0e7c6b404104159
michel <michel@randahl.net>
2015-05-03 12:20:23 +0200

    trying to fix maven jar libs

--- client/src/SkyNet/HTN/HTNUtils.java
+++ client/src/SkyNet/HTN/HTNUtils.java
@@ -2,9 +2,10 @@ package SkyNet.HTN;
 
 import SkyNet.Command;
 import SkyNet.model.Level;
+import SkyNet.model.PathFragment;
 //https://functionaljava.ci.cloudbees.com/job/master/javadoc/
 import fj.data.List;
 
 public interface HTNUtils {
-    public List<Command> findAgentMovePath(Position start, Position goal, Level level);
+    public List<Command> findAgentMovePath(PathFragment.Position start, PathFragment.Position goal, Level level);
 }
